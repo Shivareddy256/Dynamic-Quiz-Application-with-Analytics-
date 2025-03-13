8ec849d5748746cd9ffc82cb6167971aab2bd3e0
Shivareddy256 <146044661+Shivareddy256@users.noreply.github.com>
2025-03-13 01:06:59 -0500

    Update forms.py
    week 9

--- dynamic_quizz_App/forms.py
+++ dynamic_quizz_App/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
@@ -13,3 +13,20 @@ class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+
+class QuizCreationForm(FlaskForm):
+    title = StringField("Quiz Title", validators=[DataRequired()])
+    question_text = TextAreaField("Question", validators=[DataRequired()])
+    question_type = RadioField("Question Type", choices=[('MCQ', 'Multiple Choice'), ('True/False', 'True/False')], validators=[DataRequired()])
+    options = StringField("Options (Comma-separated for MCQ)")
+    correct_answer = StringField("Correct Answer", validators=[DataRequired()])
+    submit = SubmitField("Create Quiz")
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("New Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Reset Password")
